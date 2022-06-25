6d16529eaca788fac661f9e50540267570e975f0
ELSY20 <yhulii.silgado@gmail.com>
2022-06-24 21:48:58 -0500

    taller_3

---
+++ Taller_3_HW.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Elsy
+# 
+
+Elsy Yuliana Silgado Rivera
+ID: 502194
+elsy.silgado@upb.edu.co
+
+"""
+# Definimos las variables con su valor
+a=1; b=2; c=3; d=4; e=5; f=6
+
+ecu1 = (a+(b/c))/(d+(e/f))
+Ecu2 = a-(b/(c-d))
+
+Ecu3 = ecu1
+ecu1 = Ecu2
+Ecu2 = Ecu3
+
+# Imprimimos las ecuaciones
+print(ecu1)
+print(Ecu2)
